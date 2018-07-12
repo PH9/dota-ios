beffbad8659bb3f62fff025d626e7c26ac27504f
Wasith Theerapattrathamrong <phai@live.com>
2018-07-12 09:30:16 +0700

    Move Codable to ObjectMapper

--- dota/Model/Hero.swift
+++ dota/Model/Hero.swift
@@ -1,5 +1,7 @@
+import ObjectMapper
+
 // swiftlint:disable identifier_name
-struct Hero: Decodable {
+struct Hero: Decodable, ImmutableMappable {
     let id: Int
     let name: String
     let localized_name: String
@@ -7,6 +9,16 @@ struct Hero: Decodable {
     let attack_type: AttackType
     let roles: [String]
     let legs: Int
+
+    init(map: Map) throws {
+        id = try map.value("id")
+        name = try map.value("name")
+        localized_name = try map.value("localized_name")
+        primary_attr = try map.value("primary_attr")
+        attack_type = try map.value("attack_type")
+        roles = try map.value("roles")
+        legs = try map.value("legs")
+    }
 
     enum Attribute: String, Decodable {
         case agi
--- dota/Model/HeroTests.swift
+++ dota/Model/HeroTests.swift
@@ -1,5 +1,6 @@
 import XCTest
 @testable import dota
+import ObjectMapper
 
 // swiftlint:disable force_try
 class HeroTests: XCTestCase {
@@ -21,6 +22,21 @@ class HeroTests: XCTestCase {
     func testHero_fromDecodableFunc() {
         let dict = json(from: "HeroTests")!
         let hero = try! decode(JSON: dict, type: Hero.self)
+
+        XCTAssertEqual(1, hero.id)
+        XCTAssertEqual("npc_dota_hero_antimage", hero.name)
+        XCTAssertEqual("Anti-Mage", hero.localized_name)
+        XCTAssertEqual(Hero.Attribute.agi, hero.primary_attr)
+        XCTAssertEqual(Hero.AttackType.melee, hero.attack_type)
+
+        let expectedRoles = ["Carry", "Escape", "Nuker"]
+        XCTAssertEqual(expectedRoles, hero.roles)
+        XCTAssertEqual(2, hero.legs)
+    }
+
+    func testHero_fromObjectMapper() {
+        let dict = json(from: "HeroTests")!
+        let hero = try! Hero(JSONObject: dict)
 
         XCTAssertEqual(1, hero.id)
         XCTAssertEqual("npc_dota_hero_antimage", hero.name)
