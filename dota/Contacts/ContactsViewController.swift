c4cea3dbd2edac6784a4fee6cf9d614eebedfdd3
Wasith Theerapattrathamrong <phai@live.com>
2018-07-19 15:57:16 +0700

    Implement Contacts

---
+++ dota/Contacts/ContactsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ContactsViewController.swift
+//  dota
+//
+//  Created by Wasith Theerapattrathamrong on 19/7/2561 BE.
+//  Copyright © 2561 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
